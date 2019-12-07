2b3ebebd56481587463f4350fe59aee22ed77514
calfian <chrisalfian@knights.ucf.edu>
2019-12-06 20:02:32 -0500

    final

--- Tutorial 2/Assets/Scripts/playerController.cs
+++ Tutorial 2/Assets/Scripts/playerController.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using System.Collections;
 using UnityEngine.UI;
 
 public class playerController : MonoBehaviour
@@ -9,6 +10,9 @@ public class playerController : MonoBehaviour
     public Text score;
     public Text wintext;
     public Text livesText;
+    public Text timelimitText;
+    public Text powerText;
+    private float timeLimit = 60.0f;
     private int scoreValue = 0;
     private int livesValue = 3;
     public AudioSource musicSource;
@@ -18,8 +22,12 @@ public class playerController : MonoBehaviour
     public AudioClip musicClipcoin;
     public AudioClip musicClipjump;
     public AudioClip musicClipoof;
+    public AudioClip musicClippowerup;
     private bool faceRight;
+    private bool powered = false;
     private SpriteRenderer flip;
+    private IEnumerator coroutine;
+    private IEnumerator powerup;
     Animator anim;
 
 
@@ -29,12 +37,18 @@ public class playerController : MonoBehaviour
         rd2d = GetComponent<Rigidbody2D>();
         score.text = "Score: "+ scoreValue.ToString();
         wintext.text = " ";
-        livesText.text = "Lives: " +livesValue;
+        livesText.text = "Lives: "+livesValue;
+        powerText.text = "";
+        timelimitText.text = "" + timeLimit;
         musicSource.clip = musicClipsong;
         musicSource.Play();
         anim = GetComponent<Animator>();
         flip = GetComponent<SpriteRenderer>();
         faceRight = true;
+        coroutine = StartCountdown();
+        StartCoroutine(coroutine);
+        powerup = Powerup();
+        
     }
 
     // Update is called once per frame
@@ -103,6 +117,7 @@ public class playerController : MonoBehaviour
                 musicSource.Play();
                 musicSource.volume = 0.75f;
                 musicSource.loop = false;
+                StopCoroutine(coroutine);
             }
 
         }
@@ -119,8 +134,40 @@ public class playerController : MonoBehaviour
         }
 
     }
+    public IEnumerator StartCountdown(float countdownValue =30)
+    {
+        timeLimit = countdownValue;
+        
+        
+            while (timeLimit >= 0)
+            {
+                timelimitText.text = "" + timeLimit;
+                yield return new WaitForSeconds(1.0f);
+                timeLimit--;
 
-     void OnTriggerEnter2D(Collider2D collision)
+                if (timeLimit == 0)
+                {
+                    wintext.text = "You Lose! Game Created by Christopher Alfian".ToString();
+                    gameObject.SetActive(false);
+                }
+            }
+        
+ 
+    }
+    public IEnumerator Powerup(float powerupTime=2)
+    {
+        while (powerupTime >= 0)
+        {
+            powerText.text = "Double Jump Height!";
+            yield return new WaitForSeconds(1.0f);
+            powerupTime--;
+        }
+        
+        powerText.text = "";
+    }
+
+
+    void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.gameObject.CompareTag("coin"))
         {
@@ -135,13 +182,21 @@ public class playerController : MonoBehaviour
                 transform.position = new Vector2(74.0f, 1.97f);
                 livesValue = 3;
                 livesText.text = "Lives: " + livesValue.ToString();
-                score.text = scoreValue.ToString();
+                score.text = "Score: "+ scoreValue.ToString();
             }
 
         }
-        
 
-    }
+        if (collision.gameObject.CompareTag("power"))
+        {
+            musicScource2.clip = musicClippowerup;
+            musicScource2.Play();
+            collision.gameObject.SetActive(false);
+            powered = true;
+            StartCoroutine(powerup);
+        }
+
+        }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
@@ -176,12 +231,22 @@ public class playerController : MonoBehaviour
 
             if (Input.GetKey(KeyCode.W))
             {
-                rd2d.AddForce(new Vector2(0, 3), ForceMode2D.Impulse);
-                musicScource2.clip = musicClipjump;
-                musicScource2.Play();
-                anim.SetInteger("state", 2);
-                anim.SetBool("ground", false);
-
+                if (powered == true)
+                {
+                    rd2d.AddForce(new Vector2(0, 4), ForceMode2D.Impulse);
+                    musicScource2.clip = musicClipjump;
+                    musicScource2.Play();
+                    anim.SetInteger("state", 2);
+                    anim.SetBool("ground", false);
+                }
+                else
+                {
+                    rd2d.AddForce(new Vector2(0, 3), ForceMode2D.Impulse);
+                    musicScource2.clip = musicClipjump;
+                    musicScource2.Play();
+                    anim.SetInteger("state", 2);
+                    anim.SetBool("ground", false);
+                }
             }
 
            
